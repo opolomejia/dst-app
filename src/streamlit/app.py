5816127c4bbac0ffbad0713d750063fbf9e36f8e
Oliver POLO-MEJIA <olpolomejia@airfrance.fr>
2025-06-26 18:06:32 +0200

    Add demostrator

--- src/streamlit/app.py
+++ src/streamlit/app.py
@@ -8,21 +8,49 @@ import modelisation
 def main():
     
     # Show the sidebar with navigation and contact info
-    st.sidebar.title("Navigation")
-    page = st.sidebar.radio("Choose a page:", ("Introduction", "Exploration des données", "Modelisation", "Demo"))
+    st.sidebar.title("Classification de Documents")
+    page = st.sidebar.radio("Menu", ("Introduction", "Exploration des données", 
+                                              "Modelisation", "Démonstrateur"))
     
-    st.sidebar.header("Contact Info")
-    st.sidebar.write("For inquiries, please contact:")
-    st.sidebar.write("Email: contact@example.com")
-    st.sidebar.write("Phone: +123456789")
 
+    st.sidebar.header("Equipe")
+    st.sidebar.markdown("""
+    <ul>
+        <li>
+            <a href="https://www.linkedin.com/in/hiep-leconte-a7355bb4/" target="_blank">
+                <img src="https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/linkedin.svg" width="16" style="vertical-align:middle;"/> 
+                Hiep LECONTE
+            </a>
+        </li>
+        <li>
+            <a href="https://www.linkedin.com/in/oliver-polo/" target="_blank">
+                <img src="https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/linkedin.svg" width="16" style="vertical-align:middle;"/> 
+                Oliver POLO MEJIA
+            </a>
+        </li>
+        <li>
+            <a href="https://www.linkedin.com/in/somphone-sengsavang-4530823/" target="_blank">
+                <img src="https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/linkedin.svg" width="16" style="vertical-align:middle;"/>
+                Somphone SENGSAVANG
+            </a>
+        </li>
+    </ul>
+    """, unsafe_allow_html=True)
+
+    st.sidebar.header("Projet GitHub")
+    st.sidebar.markdown("""
+    <a href="https://github.com/DataScientest-Studio/sept24_cds_extractions_docs" target="_blank">
+        <img src="https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/github.svg" width="20" style="vertical-align:middle; margin-right:8px;"/>
+        Voir le projet sur GitHub
+    </a>
+    """, unsafe_allow_html=True)
     if page == "Introduction":
         home.home()
     elif page == "Exploration des données": 
         data_exploration.main()
     elif page == "Modelisation":
         modelisation.main()
-    elif page == "Demo":
+    elif page == "Démonstrateur":
         demo.demo_interface()
 
 if __name__ == "__main__":
--- src/streamlit/demo.py
+++ src/streamlit/demo.py
@@ -1,17 +1,98 @@
 import streamlit as st
+from PIL import Image
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.applications.inception_v3 import preprocess_input
+import joblib
+import warnings
+import os
+
+# Suppress TensorFlow warnings
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # Suppress INFO and WARNING logs
+warnings.filterwarnings('ignore', category=UserWarning, module='tensorflow')
+directory = "/mount/src/dst-app/src/streamlit/"
+#directory = "./"
+
+def load_cv_model():
+    model_path = directory+"models/transfert_learn_inception_v3_final.h5"
+    model = tf.keras.models.load_model(model_path, 
+           custom_objects={'preprocess_input': preprocess_input})
+    # Recompile the model to avoid the metrics warning
+    model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy')
+    return model
+
+def load_text_model():
+    model_path = directory+"models/logistic_regression_model.joblib"
+    return joblib.load(model_path)
 
 def demo_interface():
-    st.title("Model Demo")
-    st.write("Interact with the model using the inputs below.")
+    """
+    Streamlit interface for image classification demo.
+    """
+    st.title("Classification de Documents")
+    uploaded_file = st.file_uploader("Télécharger une image", type=["jpg", "jpeg", "png"])
+    global cv_model
+    global text_model
+    global document_type
+    if uploaded_file is not None:
+        img = tf.keras.preprocessing.image.load_img(
+            uploaded_file,
+            target_size=(300, 226),
+            color_mode="grayscale",
+            interpolation='bilinear'
+        )
+        st.image(img, caption="Uploaded Image", use_container_width=False)
+        cv_predict, text_predict = st.columns(2)
+        
+        if st.button("Classifier"):
+            with cv_predict:
+                img_array = tf.keras.preprocessing.image.img_to_array(img)
+                img_array = tf.expand_dims(img_array, 0)
+                st.subheader("Prédictions avec Computer Vision")
+                predictions = cv_model.predict(img_array)
+                top_3_indices = np.argsort(predictions[0])[::-1][:3]
+                top_3_probs = predictions[0][top_3_indices]
+                for idx, prob in zip(top_3_indices, top_3_probs):
+                    st.markdown(
+                        f"""
+                        <div style="line-height: 1.2;">
+                            <strong>Type de Document</strong>: {document_type[str(int(idx))]}
+                            <br>
+                            <strong>Probabilité</strong>: {prob:.4f}
+                            <br><br>
+                        </div>
+                        """, 
+                        unsafe_allow_html=True
+                    )
 
-    # Input fields for user to enter data
-    input_data = st.text_input("Enter your input data:")
-    
-    if st.button("Run Model"):
-        # Placeholder for model prediction logic
-        st.write("Model prediction results will be displayed here.")
-        # Example: result = model.predict(input_data)
-        # st.write(result)
+            with text_predict:
+                st.subheader("Prédictions avec Text Mining")
+                #ICI ON DOIT LANCER LA RECUPERATION DU TEXTE (OCR) 
+                #TRAITEMENT DU TEXTE
+                #PUIS LANCER LA PRÉDICTION AVEC LE MODELE 'text_model' DEJA CHARGÉ
+                #UNE FOIS LA PREDICTION FAITE, ON AFFICHE LES RÉSULTAT
+                #UTILISER "img"comme INPUT POUR L'EXTRACTION DU TEXTE
 
-if __name__ == "__main__":
-    demo_interface()
+
+cv_model = load_cv_model()
+text_model = load_text_model()
+
+document_type = {
+        "0": "letter",
+        "1": "form",
+        "2": "email",
+        "3": "handwritten",
+        "4": "advertisement",
+        "5": "scientific report",
+        "6": "scientific publication",
+        "7": "specification",
+        "8": "file folder",
+        "9": "news article",
+        "10": "budget",
+        "11": "invoice",
+        "12": "presentation",
+        "13": "questionnaire",
+        "14": "resume",
+        "15": "memo"
+    }
